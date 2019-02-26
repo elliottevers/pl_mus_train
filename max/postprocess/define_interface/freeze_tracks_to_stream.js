d38d6411419d13d61717549984829ae264a187c1
Elliott Evers <everselliott@gmail.com>
2019-02-26 09:38:44 -0700

    cleanu

--- max/postprocess/define_interface/freeze_tracks_to_stream.js
+++
@@ -1,4 +0,0 @@
-var test = Global.freeze_tracks_to_stream.test;
-var add = Global.freeze_tracks_to_stream.add;
-var remove = Global.freeze_tracks_to_stream.remove;
-var export_clips = Global.freeze_tracks_to_stream.export_clips;
