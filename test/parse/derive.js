d8886d3b70f457cb73d8397802476f51a6313bf4
Elliott Evers <everselliott@gmail.com>
2019-03-05 19:51:33 -0700

    cleaning up

--- test/parse/derive.js
+++ test/parse/derive.js
@@ -5,9 +5,7 @@ var TreeModel = require("tree-model");
 var parse_tree_1 = require("../../src/scripts/parse_tree");
 var messenger_1 = require("../../src/message/messenger");
 var Messenger = messenger_1.message.Messenger;
-// import {song} from "../../src/song/song";
-// import Song = song.Song;
-// import SongDao = song.SongDao;
+// stubs
 var song = {
     set_overdub: function (int) { },
     set_session_record: function (int) { }
@@ -19,21 +17,6 @@ var clip_user_input = {
 };
 var tree = new TreeModel();
 var notes_segments;
-// let note_1_1 = tree.parse(
-//     {
-//         id: -1, // TODO: hashing scheme for clip id and beat start
-//         note: new n.Note(
-//             48,
-//             0,
-//             48 + 16,
-//             90,
-//             0
-//         ),
-//         children: [
-//
-//         ]
-//     }
-// );
 var note_2_1 = tree.parse({
     id: -1,
     note: new note_1.note.Note(48, 0, 16, 90, 0),
@@ -84,30 +67,21 @@ var note_4_1 = tree.parse({
     note: new note_1.note.Note(51, 7, 1, 90, 0),
     children: []
 });
+var note_4_2 = tree.parse({
+    id: -1,
+    note: new note_1.note.Note(51, 25, 3, 90, 0),
+    children: []
+});
 notes_segments = [note_2_1, note_2_2, note_2_3];
-// notes_segments = [note_2_1, note_2_2];
 parse_tree_1.set_depth_tree_export(4);
 var env;
 env = 'node';
 env = 'node_for_max';
-// let messenger = new Messenger(node, 0);
 var messenger = new Messenger(env, 0);
 parse_tree_1.begin_train_export(notes_segments, clip_user_input, song, parse_tree_1.add_to_tree_export, messenger);
-// add_to_tree(
-//     note_1_1
-// )
-//
 parse_tree_1.add_to_tree_export([note_3_1, note_3_2], note_2_1.model.note.beat_start, note_2_1.model.note.get_beat_end(), clip_user_input, song, messenger);
 parse_tree_1.add_to_tree_export([note_3_3, note_3_4], note_2_2.model.note.beat_start, note_2_2.model.note.get_beat_end(), clip_user_input, song, messenger);
 parse_tree_1.add_to_tree_export([note_3_5, note_3_6], note_2_3.model.note.beat_start, note_2_3.model.note.get_beat_end(), clip_user_input, song, messenger);
 parse_tree_1.add_to_tree_export([note_4_1], note_2_1.model.note.beat_start, note_2_1.model.note.get_beat_end(), clip_user_input, song, messenger);
-// add_to_tree(note_2_2)
-// add_to_tree(note_2_3)
-// add_to_tree(note_3_1)
-// add_to_tree(note_3_2)
-// add_to_tree(note_3_3)
-//
-// add_to_tree(note_3_4)
-// add_to_tree(note_3_5)
-// add_to_tree(note_3_6)
+parse_tree_1.add_to_tree_export([note_4_2], note_2_2.model.note.beat_start, note_2_2.model.note.get_beat_end(), clip_user_input, song, messenger);
 //# sourceMappingURL=derive.js.map
--- test/parse/derive.ts
+++ test/parse/derive.ts
@@ -1,15 +1,12 @@
-import sinon = require("sinon");
 import {note as n, note} from "../../src/note/note";
-import Note = note.Note;
 import TreeModel = require("tree-model");
 import {begin_train_export, set_depth_tree_export, add_to_tree_export} from "../../src/scripts/parse_tree";
 import {message} from "../../src/message/messenger";
 import Messenger = message.Messenger;
-// import {song} from "../../src/song/song";
-// import Song = song.Song;
-// import SongDao = song.SongDao;
 
 
+
+// stubs
 let song = {
     set_overdub: (int) => {},
     set_session_record: (int) => {}
@@ -25,22 +22,6 @@ let clip_user_input = {
 let tree: TreeModel = new TreeModel();
 
 let notes_segments;
-
-// let note_1_1 = tree.parse(
-//     {
-//         id: -1, // TODO: hashing scheme for clip id and beat start
-//         note: new n.Note(
-//             48,
-//             0,
-//             48 + 16,
-//             90,
-//             0
-//         ),
-//         children: [
-//
-//         ]
-//     }
-// );
 
 let note_2_1 = tree.parse(
     {
@@ -202,25 +183,35 @@ let note_4_1 = tree.parse(
     }
 );
 
+let note_4_2 = tree.parse(
+    {
+        id: -1, // TODO: hashing scheme for clip id and beat start
+        note: new n.Note(
+            51,
+            25,
+            3,
+            90,
+            0
+        ),
+        children: [
+
+        ]
+    }
+);
+
 notes_segments = [note_2_1, note_2_2, note_2_3];
 
-// notes_segments = [note_2_1, note_2_2];
-
 set_depth_tree_export(4);
+
 let env: string;
 env = 'node';
 env = 'node_for_max';
 
-// let messenger = new Messenger(node, 0);
 let messenger = new Messenger(env, 0);
 
 
 begin_train_export(notes_segments, clip_user_input, song, add_to_tree_export, messenger);
 
-// add_to_tree(
-//     note_1_1
-// )
-//
 add_to_tree_export(
     [note_3_1, note_3_2],
     note_2_1.model.note.beat_start,
@@ -256,13 +247,12 @@ add_to_tree_export(
     song,
     messenger
 );
-// add_to_tree(note_2_2)
-// add_to_tree(note_2_3)
 
-// add_to_tree(note_3_1)
-// add_to_tree(note_3_2)
-// add_to_tree(note_3_3)
-//
-// add_to_tree(note_3_4)
-// add_to_tree(note_3_5)
-// add_to_tree(note_3_6)
+add_to_tree_export(
+    [note_4_2],
+    note_2_2.model.note.beat_start,
+    note_2_2.model.note.get_beat_end(),
+    clip_user_input,
+    song,
+    messenger
+);
