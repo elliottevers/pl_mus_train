eddb2acb4268577e051c748740fc5312ad0f504b
Elliott Evers <everselliott@gmail.com>
2019-03-23 18:45:33 -0600

    test case predict compiles

--- test/classify/predict.js
+++ test/classify/predict.js
@@ -6,31 +6,42 @@ var user_input_1 = require("../../src/control/user_input");
 var UserInputHandler = user_input_1.user_input.UserInputHandler;
 var messenger_1 = require("../../src/message/messenger");
 var Messenger = messenger_1.message.Messenger;
-var segment_1 = require("../../src/segment/segment");
-var Segment = segment_1.segment.Segment;
+var live_1 = require("../../src/live/live");
+var LiveClipVirtual = live_1.live.LiveClipVirtual;
+var clip_1 = require("../../src/clip/clip");
+var Clip = clip_1.clip.Clip;
 var algorithm_1 = require("../../src/train/algorithm");
 var window_1 = require("../../src/render/window");
 var trainer_1 = require("../../src/train/trainer");
 var Trainer = trainer_1.trainer.Trainer;
 var constants_1 = require("../../src/constants/constants");
+var POLYPHONY = constants_1.modes_texture.POLYPHONY;
 var INSTRUMENTAL = constants_1.modes_control.INSTRUMENTAL;
 var MatrixWindow = window_1.window.MatrixWindow;
-var MONOPHONY = constants_1.modes_texture.MONOPHONY;
 var Predict = algorithm_1.algorithm.Predict;
+var scene_1 = require("../../src/scene/scene");
+var SceneDaoVirtual = scene_1.scene.SceneDaoVirtual;
+var track_1 = require("../../src/track/track");
+var TrackDaoVirtual = track_1.track.TrackDaoVirtual;
+var song_1 = require("../../src/song/song");
+var SongDaoVirtual = song_1.song.SongDaoVirtual;
+var Scene = scene_1.scene.Scene;
+var Song = song_1.song.Song;
+var Track = track_1.track.Track;
 var tree = new TreeModel();
 var segment_note_1 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(51, 1, 4, 90, 0),
+    note: new note_1.note.Note(51, 0, 4, 90, 0),
     children: []
 });
 var segment_note_2 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(51, 5, 4, 90, 0),
+    note: new note_1.note.Note(51, 4, 4, 90, 0),
     children: []
 });
 var note_target_1_subtarget_1 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(51, 1, 2, 90, 0),
+    note: new note_1.note.Note(51, 0, 2, 90, 0),
     children: []
 });
 // let note_target_1_subtarget_2 = tree.parse(
@@ -50,7 +61,7 @@ var note_target_1_subtarget_1 = tree.parse({
 // );
 var note_target_2_subtarget_1 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(52, 3, 2, 90, 0),
+    note: new note_1.note.Note(52, 2, 2, 90, 0),
     children: []
 });
 // let note_target_2_subtarget_2 = tree.parse(
@@ -70,7 +81,7 @@ var note_target_2_subtarget_1 = tree.parse({
 // );
 var note_target_3_subtarget_1 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(53, 5, 2, 90, 0),
+    note: new note_1.note.Note(53, 4, 2, 90, 0),
     children: []
 });
 // let note_target_3_subtarget_2 = tree.parse(
@@ -90,7 +101,7 @@ var note_target_3_subtarget_1 = tree.parse({
 // );
 var note_target_4_subtarget_1 = tree.parse({
     id: -1,
-    note: new note_1.note.Note(54, 7, 2, 90, 0),
+    note: new note_1.note.Note(54, 6, 2, 90, 0),
     children: []
 });
 // let note_target_4_subtarget_2 = tree.parse(
@@ -108,56 +119,59 @@ var note_target_4_subtarget_1 = tree.parse({
 //         ]
 //     }
 // );
-var mode_texture = MONOPHONY;
+var mode_texture = POLYPHONY;
 var mode_control = INSTRUMENTAL;
 var user_input_handler = new UserInputHandler(mode_texture, mode_control);
 var env = 'node_for_max';
 // env = 'node';
 var messenger = new Messenger(env, 0, 'render_predict');
-var algorithm_train = new Predict(user_input_handler);
-var window_local = new MatrixWindow(384, 384, messenger, algorithm_train);
-// stubs
-var song = {
-    set_overdub: function (int) { },
-    set_session_record: function (int) { },
-    stop: function () { }
-};
-var clip_user_input = {
-    fire: function () { },
-    stop: function () { },
-    set_endpoints_loop: function (former, latter) { },
-    set_notes: function () { },
-    remove_notes: function () { }
-};
-var clip_user_input_synchronous = {
-    fire: function () { },
-    stop: function () { },
-    set_endpoints_loop: function (former, latter) { }
-};
-var notes_segments = [
-    segment_note_1,
-    segment_note_2
-];
-var notes_target_clip = [
+var algorithm_train = new Predict();
+var window_train = new MatrixWindow(384, 384, messenger);
+var scene, scenes;
+scenes = [];
+scene = new Scene(new SceneDaoVirtual());
+scenes.push(scene);
+scene = new Scene(new SceneDaoVirtual());
+scenes.push(scene);
+var song = new Song(new SongDaoVirtual(scenes));
+// let num_clip_slots = 2;
+// USER INPUT CLIP - HAS THE SEGMENTS
+var clip_dao_virtual, clip_user_input;
+var clips_user_input = [];
+clip_dao_virtual = new LiveClipVirtual([segment_note_1]);
+clip_dao_virtual.beat_start = 0;
+clip_dao_virtual.beat_end = 4;
+clip_user_input = new Clip(clip_dao_virtual);
+clips_user_input.push(clip_user_input);
+clip_dao_virtual = new LiveClipVirtual([segment_note_2]);
+clip_dao_virtual.beat_start = 4;
+clip_dao_virtual.beat_end = 8;
+clip_user_input = new Clip(clip_dao_virtual);
+clips_user_input.push(clip_user_input);
+var track_user_input = new Track(new TrackDaoVirtual(clips_user_input));
+// TARGET CLIP
+var clips_target = [];
+var clip_target;
+clip_dao_virtual = new LiveClipVirtual([
     note_target_1_subtarget_1,
-    // note_target_1_subtarget_2,
     note_target_2_subtarget_1,
-    // note_target_2_subtarget_2,
+]);
+clip_dao_virtual.beat_start = 0;
+clip_dao_virtual.beat_end = 4;
+clip_target = new Clip(clip_dao_virtual);
+clips_target.push(clip_target);
+clip_dao_virtual = new LiveClipVirtual([
     note_target_3_subtarget_1,
-    // note_target_3_subtarget_2,
     note_target_4_subtarget_1,
-];
-var segments = [];
-for (var _i = 0, notes_segments_1 = notes_segments; _i < notes_segments_1.length; _i++) {
-    var note = notes_segments_1[_i];
-    segments.push(new Segment(note));
-}
-// let clip_dao_virtual = new LiveClipVirtual(notes_target_clip);
-//
-// let clip_target = new Clip(clip_dao_virtual);
-var trainer_local = new Trainer(window_local, user_input_handler, algorithm_train, clip_user_input, clip_user_input_synchronous, notes_target_clip, song, segments, messenger);
+]);
+clip_dao_virtual.beat_start = 4;
+clip_dao_virtual.beat_end = 8;
+clip_target = new Clip(clip_dao_virtual);
+clips_target.push(clip_target);
+var track_target = new Track(new TrackDaoVirtual(clips_target));
+var trainer_local = new Trainer(window_train, user_input_handler, algorithm_train, track_target, track_user_input, song, messenger);
 // test case - 2 segments, 2 notes a piece
-trainer_local.init();
+trainer_local.commence();
 trainer_local.accept_input([note_target_1_subtarget_1]);
 // trainer_local.accept_input(
 //     [note_target_1_subtarget_2]
--- test/classify/predict.ts
+++ test/classify/predict.ts
@@ -6,12 +6,9 @@ import {message} from "../../src/message/messenger";
 import Messenger = message.Messenger;
 import {live} from "../../src/live/live";
 import LiveClipVirtual = live.LiveClipVirtual;
-import {segment} from "../../src/segment/segment";
-import Segment = segment.Segment;
 import {clip} from "../../src/clip/clip";
 import Clip = clip.Clip;
 import {algorithm} from "../../src/train/algorithm";
-import Detect = algorithm.Detect;
 import {freeze, thaw} from "../../src/serialize/serialize";
 import TrainFreezer = freeze.TrainFreezer;
 import TrainThawer = thaw.TrainThawer;
@@ -22,8 +19,16 @@ import {modes_control, modes_texture} from "../../src/constants/constants";
 import POLYPHONY = modes_texture.POLYPHONY;
 import INSTRUMENTAL = modes_control.INSTRUMENTAL;
 import MatrixWindow = window.MatrixWindow;
-import MONOPHONY = modes_texture.MONOPHONY;
 import Predict = algorithm.Predict;
+import {scene as module_scene} from "../../src/scene/scene";
+import SceneDaoVirtual = module_scene.SceneDaoVirtual;
+import {track} from "../../src/track/track";
+import TrackDaoVirtual = track.TrackDaoVirtual;
+import {song as module_song} from "../../src/song/song";
+import SongDaoVirtual = module_song.SongDaoVirtual;
+import Scene = module_scene.Scene;
+import Song = module_song.Song;
+import Track = track.Track;
 
 
 let tree: TreeModel = new TreeModel();
@@ -33,7 +38,7 @@ let segment_note_1 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             51,
-            1,
+            0,
             4,
             90,
             0
@@ -49,7 +54,7 @@ let segment_note_2 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             51,
-            5,
+            4,
             4,
             90,
             0
@@ -67,7 +72,7 @@ let note_target_1_subtarget_1 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             51,
-            1,
+            0,
             2,
             90,
             0
@@ -101,7 +106,7 @@ let note_target_2_subtarget_1 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             52,
-            3,
+            2,
             2,
             90,
             0
@@ -135,7 +140,7 @@ let note_target_3_subtarget_1 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             53,
-            5,
+            4,
             2,
             90,
             0
@@ -169,7 +174,7 @@ let note_target_4_subtarget_1 = tree.parse(
         id: -1, // TODO: hashing scheme for clip id and beat start
         note: new n.Note(
             54,
-            7,
+            6,
             2,
             90,
             0
@@ -197,7 +202,7 @@ let note_target_4_subtarget_1 = tree.parse(
 // );
 
 
-let mode_texture = MONOPHONY;
+let mode_texture = POLYPHONY;
 let mode_control = INSTRUMENTAL;
 
 let user_input_handler = new UserInputHandler(
@@ -212,82 +217,146 @@ let env: string = 'node_for_max';
 let messenger = new Messenger(env, 0, 'render_predict');
 
 let algorithm_train = new Predict(
-    user_input_handler
 );
 
-let window_local = new MatrixWindow(
+let window_train = new MatrixWindow(
     384,
     384,
-    messenger,
-    algorithm_train
+    messenger
 );
 
-// stubs
-let song = {
-    set_overdub: (int) => {},
-    set_session_record: (int) => {},
-    stop: () => {}
-};
+let scene, scenes;
 
-let clip_user_input = {
-    fire: () => {},
-    stop: () => {},
-    set_endpoints_loop: (former, latter) => {},
-    set_notes: () => {},
-    remove_notes: () => {}
-};
+scenes = [];
 
-let clip_user_input_synchronous = {
-    fire: () => {},
-    stop: () => {},
-    set_endpoints_loop: (former, latter) => {}
-};
+scene = new Scene(
+    new SceneDaoVirtual(
 
-let notes_segments = [
-    segment_note_1,
-    segment_note_2
-];
-
-let notes_target_clip = [
-    note_target_1_subtarget_1,
-    // note_target_1_subtarget_2,
-    note_target_2_subtarget_1,
-    // note_target_2_subtarget_2,
-    note_target_3_subtarget_1,
-    // note_target_3_subtarget_2,
-    note_target_4_subtarget_1,
-    // note_target_4_subtarget_2
-];
-
-let segments: Segment[] = [];
-
-for (let note of notes_segments) {
-    segments.push(
-        new Segment(
-            note
-        )
     )
-}
+);
 
-// let clip_dao_virtual = new LiveClipVirtual(notes_target_clip);
-//
-// let clip_target = new Clip(clip_dao_virtual);
+scenes.push(scene);
+
+scene = new Scene(
+    new SceneDaoVirtual(
+
+    )
+);
+
+scenes.push(scene);
+
+let song = new Song(
+    new SongDaoVirtual(
+        scenes
+    )
+);
+
+// let num_clip_slots = 2;
+
+
+// USER INPUT CLIP - HAS THE SEGMENTS
+
+let clip_dao_virtual, clip_user_input;
+
+let clips_user_input = [];
+
+
+clip_dao_virtual = new LiveClipVirtual([segment_note_1]);
+
+clip_dao_virtual.beat_start = 0;
+
+clip_dao_virtual.beat_end = 4;
+
+clip_user_input = new Clip(
+    clip_dao_virtual
+);
+
+clips_user_input.push(clip_user_input);
+
+
+
+clip_dao_virtual = new LiveClipVirtual([segment_note_2]);
+
+clip_dao_virtual.beat_start = 4;
+
+clip_dao_virtual.beat_end = 8;
+
+clip_user_input = new Clip(
+    clip_dao_virtual
+);
+
+clips_user_input.push(clip_user_input);
+
+
+
+let track_user_input = new Track(
+    new TrackDaoVirtual(
+        clips_user_input
+    )
+);
+
+// TARGET CLIP
+
+let clips_target = [];
+
+let clip_target;
+
+clip_dao_virtual = new LiveClipVirtual(
+    [
+        note_target_1_subtarget_1,
+        note_target_2_subtarget_1,
+    ]
+);
+
+clip_dao_virtual.beat_start = 0;
+
+clip_dao_virtual.beat_end = 4;
+
+clip_target = new Clip(
+    clip_dao_virtual
+);
+
+clips_target.push(clip_target);
+
+
+
+clip_dao_virtual = new LiveClipVirtual(
+    [
+        note_target_3_subtarget_1,
+        note_target_4_subtarget_1,
+    ]
+);
+
+clip_dao_virtual.beat_start = 4;
+
+clip_dao_virtual.beat_end = 8;
+
+clip_target = new Clip(
+    clip_dao_virtual
+);
+
+clips_target.push(clip_target);
+
+
+let track_target = new Track(
+    new TrackDaoVirtual(
+        clips_target
+    )
+);
 
 let trainer_local = new Trainer(
-    window_local,
+    window_train,
     user_input_handler,
     algorithm_train,
-    clip_user_input,
-    clip_user_input_synchronous,
-    notes_target_clip,
+    track_target,
+    track_user_input,
     song,
-    segments,
     messenger
 );
 
 // test case - 2 segments, 2 notes a piece
 
-trainer_local.init(
+trainer_local.commence(
 
 );
 
