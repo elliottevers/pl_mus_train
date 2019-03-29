60da2bd22a975d4ec80beafb63eb625c8e599de0
Elliott Evers <everselliott@gmail.com>
2019-03-29 13:25:49 -0400

    experimenting with removing compiled files within src from git

--- src/log/logger.d.ts
+++
@@ -1,9 +0,0 @@
-export declare namespace log {
-    class Logger {
-        env: string;
-        constructor(env: string);
-        log(message: string): void;
-        log_max(...args: any[]): void;
-        log_node(...args: any[]): void;
-    }
-}
--- src/parse/parse.d.ts
+++
@@ -1,6 +0,0 @@
-export declare namespace parse {
-    interface Parsable {
-        choose(): boolean;
-        get_best_candidate(list_candidate_note: any): any;
-    }
-}
