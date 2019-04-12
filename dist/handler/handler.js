d21383b6fced57297f459091029bb93247d29c80
Elliott Evers <everselliott@gmail.com>
2019-04-12 08:11:59 -0600

    removing dist folder

--- dist/handler/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var handler;
-(function (handler) {
-    handler.add_handler = function (Max, name, callback) {
-        Max.addHandler(name, callback);
-    };
-})(handler = exports.handler || (exports.handler = {}));
-//# sourceMappingURL=handler.js.map
--- dist/message/max-api.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Max = require('max-api');
-//# sourceMappingURL=max-api.js.map
