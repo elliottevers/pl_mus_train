53744ff6c801f5ca9cf0fd03d4dcf62e8538835f
Elliott Evers <everselliott@gmail.com>
2019-09-29 20:17:19 -0600

    DANGEROUS: deleting phrase module, and experimenting with serializing circular structures

--- src/scripts_node/nodeObjects.DanceLooper.ts
+++ src/scripts_node/nodeObjects.DanceLooper.ts
@@ -1,5 +1,6 @@
 export {}
 const max_api = require('max-api');
+var serialize = require('node-serialize');
 
 
 // let receive_message_saga = (name_saga, val_saga) => {
@@ -31,4 +32,40 @@ let saga_dance = function* () {
 
 max_api.addHandler("start_saga_dance", () => {
     saga_dance.next();
+});
+
+max_api.addHandler("serialize", () => {
+    // saga_dance.next();
+    // var obj = {
+    //     name: 'Bob',
+    //     say: function() {
+    //         return 'hi ' + this.name;
+    //     }
+    // };
+
+
+    // typeof objS === 'string';
+    // serialize.unserialize(objS).say() === 'hi Bob';
+    // max_api.post(serialize.unserialize(objS).say());
+
+    var TreeModel = require('tree-model');
+    var tree = new TreeModel();
+    var root = tree.parse({name: 'a', children: [{name: 'b'}]});
+
+    // root.getPath();
+
+    // JSON.stringify(root);
+
+    var objS = serialize.serialize(root.model);
+
+    // max_api.post(tree.parse(serialize.unserialize(objS)));
+
+    max_api.post(tree.parse(serialize.unserialize(objS)).getIndex());
+
+    // serialize.unserialize(objS).get_path()
+
+    // max_api.post(root.model);
+
+    // root.getPath()
+    // serialize.unserialize(objS);
 });
