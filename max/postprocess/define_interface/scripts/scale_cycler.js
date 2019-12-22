22103f8b78d2da4a8b80d143ce79441a5924eeb6
Elliott Evers <everselliott@gmail.com>
2019-12-22 13:20:50 -0700

    more useful scale patterns

--- max/postprocess/define_interface/scripts/scale_cycler.js
+++ max/postprocess/define_interface/scripts/scale_cycler.js
@@ -2,3 +2,4 @@ var right = Global.scale_cycler.right;
 var left = Global.scale_cycler.left;
 var setScale = Global.scale_cycler.setScale;
 var nextPitch = Global.scale_cycler.nextPitch;
+var resetPitch = Global.scale_cycler.resetPitch;
